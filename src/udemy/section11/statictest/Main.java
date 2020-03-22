d450f32624a8670c66ec3dd2332a14f90519fd3c
Jagmandeep <jagmandeep.dhaliwal@wework.com>
2020-03-22 17:00:15 -0400

    more static and final

--- src/udemy/section11/statictest/Main.java
+++ src/udemy/section11/statictest/Main.java
@@ -4,11 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StaticTest firstInstance = new StaticTest("1st instance");
-        System.out.println(firstInstance.getName() + " is Instance number : " + StaticTest.getNumInstances());
-
+       StaticTest firstInstance = new StaticTest("1st instance");
+//     System.out.println(firstInstance.getName() + " is Instance number : " + StaticTest.getNumInstances());
+//
         StaticTest secondInstance = new StaticTest("2ndst instance");
-        System.out.println(secondInstance.getName() + " is Instance number : " + StaticTest.getNumInstances());
+//      System.out.println(secondInstance.getName() + " is Instance number : " + StaticTest.getNumInstances());
+
+        StaticTest thirdInstance = new StaticTest("3rd instance");
+
+        System.out.println(StaticTest.getNumInstances());
+        System.out.println(StaticTest.getNumInstances());
+        System.out.println(StaticTest.getNumInstances());
+
+
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+
 
 
     }
